9daf8d5883a4516726e8aaf7fc5e8b93d0534416
James Haff <jameshaff@Jamess-MacBook-Pro.local>
2020-11-19 11:03:30 -0500

    added passport code

---
+++ work/w5/d4/passport-boilerplate/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/students', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+// database connection event
+db.on('connected', function () {
+  console.log(`Mongoose connected to: ${db.host}:${db.port}`);
+});
---
+++ work/w5/d4/passport-boilerplate/models/student.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+// The factSchema is used to embedded docs in as tudent doc.
+// There is no model and no 'facts' collection
+const factSchema = new mongoose.Schema({
+  text: String
+}, {
+  timestamps: true
+});
+
+const studentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  cohort: String,
+  avatar: String,
+  facts: [factSchema],
+  googleId: String
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model('Student', studentSchema);
