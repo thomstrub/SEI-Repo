bd6b9a4f627161a5d7108f8c522be3fb8c9cea98
James Haff <jameshaff@Jamess-MacBook-Pro.local>
2020-12-03 11:47:12 -0500

    added d3

---
+++ work/w6/d3/01-python-intro/intro_python.py
@@ -0,0 +1,47 @@
+# """
+# This is sometimes called a docstring as well
+# # """
+
+# # kinda act like let in js
+# first_name = 'Jim' # snake_case is the convention
+# # for naming in python
+# print(first_name)
+
+# first_name = 'Kate'
+# print(first_name)
+
+# lastName = 'haff' # bad python
+# print(lastName)
+
+# print(type([]))
+# print(type(4.34))
+# print(type(4))
+# print(type(False))
+
+# age = 99
+
+# "My age is " + str(age)
+
+
+# multiline_string = '''This is my string that
+#                         goes on multiple lines
+#                           for whatever reason'''
+
+# print(multiline_string)
+
+
+state = "Hawaii"
+year = 1959
+message = f"{state} was the last state to join the U.S. in {year}."
+
+
+# print(message)
+print(list("ace of spades"))
+
+print("ace of spades"[4])
+
+
+
+
+
+
---
+++ work/w6/d3/02-python-control-flow/exercise.py
@@ -0,0 +1,38 @@
+# color = input('Enter "green", "yellow", "red": ').lower()
+# print(f'The user entered {color}')
+
+
+
+
+# Branching & Looping Exercises
+
+ #
+
+
+# for n in "taco":
+#     print(n)
+
+# floor = "sticky"
+# walls = "clean"
+# if floor == "sticky":   # don't forget the colon
+#    print("Clean the floor! It's sticky!")
+#    print('this is ugly')
+#   # more lines of code in this code
+#   # block need to be indented as well
+# if walls == "sticky":
+#   print("Clean the walls! They're sticky!")
+
+names = ["Tom", "Deborah", "Murray", "Axel"]
+
+for name in names: # choosing to call each item name
+#   print(name) # notice the indention
+  if name == "Murray":
+      print(name)
+      break
+
+for item in names:
+    if name == "Murray":
+        print(name)
+        break 
+# for item in names:
+#     print(item)
